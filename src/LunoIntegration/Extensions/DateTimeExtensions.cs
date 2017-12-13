88ef6d136ee8aebba94a512c0568d2d471117bf4
louislewis2 <louis@lewisonline.co.za>
2017-12-13 10:09:41 +0200

    fix: fix datetime / unix time conversion
    missed the part in the api documentation where the time was in milliseconds

--- src/LunoIntegration/Extensions/DateTimeExtensions.cs
+++ src/LunoIntegration/Extensions/DateTimeExtensions.cs
@@ -5,7 +5,7 @@
         public static double DateTimeToUnixTimestamp(this DateTime dateTime)
         {
             return (TimeZoneInfo.ConvertTimeToUtc(dateTime) -
-                   new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+                   new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
         }
     }
 }
--- src/LunoIntegration/Extensions/DoubleExtensions.cs
+++ src/LunoIntegration/Extensions/DoubleExtensions.cs
@@ -5,7 +5,7 @@
         public static DateTime UnixTimeStampToDateTime(this double unixTimeStamp)
         {
             System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            dtDateTime = dtDateTime.AddMilliseconds(unixTimeStamp).ToLocalTime();
 
             return dtDateTime;
         }
